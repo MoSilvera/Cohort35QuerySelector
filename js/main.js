a770f0d35fec54453d3b8375c2ab7e1071203652
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-08-26 09:44:04 -0500

    ../ ''

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 console.log("Hi There")
 let firstSection = document.querySelector(".article__header")
 firstSection.textContent = "WELCOME TO THE MO ZONE"
+let dashed = document.querySelector(".dashed")
+dashed.classList.remove("dashed")
 let footer = document.querySelector(".article__footer")
 footer.classList.add("goldenrod")
