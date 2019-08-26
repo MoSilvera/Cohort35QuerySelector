7c6325f42f92eb5aa318d0945632828867951e52
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-08-26 09:31:31 -0500

    working through exercises

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 console.log("Hi There")
+let firstSection = document.querySelector(".article__header")
+firstSection.textContent = "WELCOME TO THE MO ZONE"
+let footer = document.querySelector(".article__footer")
+footer.classList.add("goldenrod")
